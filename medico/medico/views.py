564f452c6ec01ad63938f8a6050dd9bbea993455
Corruption13 <sandeey999@gmail.com>
2019-09-01 16:28:45 +0530

    Added base django

--- medico/medico/views.py
+++ medico/medico/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home_view(request):
     return HttpResponse("<h1>Hello WOrld</h1>")
+
+
+#def home_view(request):
+ #   return render()
