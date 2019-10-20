56bf325f3542825d65616de9e2f8219e11169d83
Corruption13 <sandeey999@gmail.com>
2019-10-20 17:45:54 +0530

    Input logic added

--- medico/medico/views.py
+++ medico/medico/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-def home_view(request):
-    return HttpResponse("<h1>Hello WOrld</h1>")
-
 
 #def home_view(request):
- #   return render()
+#    return HttpResponse("<h1>Hello WOrld</h1>")
+
+
+
+def home_view(request):
+    return render(request, "add-medicine.html")
